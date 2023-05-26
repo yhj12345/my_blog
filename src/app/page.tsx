a1c6aa4b60c1ca8d22545c3ce4d58f07dc6056c2
hojun <ghwnsdl999@naver.com>
2023-05-27 00:24:57 +0900

    [FE] refact: featuredPosts 컴포넌트 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,11 @@
+import FeaturedPosts from "@/components/FeaturedPosts";
 import Profile from "@/components/Profile";
-import Image from "next/image";
-import reviewImage from "../../public/images/posts/review-2023.png";
 
 export default function Home() {
   return (
     <section className="w-full">
       <Profile />
-      <div className="max-w-screen-xl mx-auto">
-        <h3 className="font-bold">Featured Posts</h3>
-        <div className="grid grid-cols-3 gap-4">
-          <div className="rounded border-2 border-gray-300">
-            <Image src={reviewImage} alt="thumbnail" />
-            <div className="text-right">날짜</div>
-            <div className="flex flex-col items-center">
-              <div>제목</div>
-              <div>부제목</div>
-              <div>태그</div>
-            </div>
-          </div>
-          <div className="rounded border-2 border-gray-300">
-            <Image src={reviewImage} alt="thumbnail" />
-            <div className="text-right">날짜</div>
-            <div className="flex flex-col items-center">
-              <div>제목</div>
-              <div>부제목</div>
-              <div>태그</div>
-            </div>
-          </div>
-          <div className="rounded border-2 border-gray-300">
-            <Image src={reviewImage} alt="thumbnail" />
-            <div className="text-right">날짜</div>
-            <div className="flex flex-col items-center">
-              <div>제목</div>
-              <div>부제목</div>
-              <div>태그</div>
-            </div>
-          </div>
-          <div className="rounded border-2 border-gray-300">
-            <Image src={reviewImage} alt="thumbnail" />
-            <div className="text-right">날짜</div>
-            <div className="flex flex-col items-center">
-              <div>제목</div>
-              <div>부제목</div>
-              <div>태그</div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <FeaturedPosts />
     </section>
   );
 }
