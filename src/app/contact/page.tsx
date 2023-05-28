33040dddc73e3ae5fedeceed55964f59ed2c76cd
hojun <ghwnsdl999@naver.com>
2023-05-28 16:10:51 +0900

    [FE] add : ContactMe 컴포넌트 제작

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="w-full">
+      <div className="max-w-screen-xl mx-auto flex flex-col items-center">
+        <h1 className="text-2xl font-bold">Contact Me</h1>
+        <p>ghwnsdl999@naver.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default page;
