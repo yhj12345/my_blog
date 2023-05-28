e0d7ef0bc73a3e89018e70d0c6865dbe2216bdf5
hojun <ghwnsdl999@naver.com>
2023-05-28 16:22:41 +0900

    [FE] style: flex center

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,8 +3,9 @@ import ContactMe from "@/components/ContactMe";
 const page = () => {
   return (
     <div className="w-full">
-      <div className="max-w-screen-xl mx-auto flex flex-col items-center">
+      <div className="max-w-screen-xl mx-auto">
         <ContactMe />
+        <h1 className="text-2xl font-bold">Or Send me an email</h1>
       </div>
     </div>
   );
--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -2,7 +2,7 @@ import { BsGithub } from "react-icons/bs";
 
 const ContactMe = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <h1 className="text-2xl font-bold">Contact Me</h1>
       <p>ghwnsdl999@naver.com</p>
       <BsGithub size="24" />
