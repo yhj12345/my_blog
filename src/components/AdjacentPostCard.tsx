9e598ce84cc604a6a6786dc89046d185bcb49dc9
hojun <ghwnsdl999@naver.com>
2023-06-02 23:13:00 +0900

    [FE] add : 배너형 Card 컴포넌트 제작

---
+++ src/components/AdjacentPostCard.tsx
@@ -0,0 +1,37 @@
+import { Post } from "@/app/api/posts";
+import Image from "next/image";
+import Link from "next/link";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
+interface Props {
+  post: Post;
+  type: "prev" | "next";
+}
+
+const ICON_CLASS =
+  "text-5xl mx-4 text-yellow-300 transition-all group-hover:text-6xl";
+
+const AdjacentPostCard = ({ post, type }: Props) => {
+  const { path, title, description } = post;
+  return (
+    <Link href={`/posts/${path}`} className="relative w-full bg-black max-h-56">
+      <Image
+        className="w-full opacity-40"
+        src={`/images/posts/${path}.png`}
+        alt=""
+        width={300}
+        height={100}
+      />
+      <div className="group absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full text-white flex items-center justify-around px-8">
+        {type === "prev" && <FaArrowLeft className={ICON_CLASS} />}
+        <div className="w-full text-center">
+          <h3 className="text-3xl font-bold">{title}</h3>
+          <p className="font-bold">{description}</p>
+        </div>
+        {type === "next" && <FaArrowRight className={ICON_CLASS} />}
+      </div>
+    </Link>
+  );
+};
+
+export default AdjacentPostCard;
