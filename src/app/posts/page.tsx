7eac06e9b57b12f4b728214d09880503d56611b4
hojun <ghwnsdl999@naver.com>
2023-06-03 17:58:38 +0900

    [FE] refact : FilterablePosts 컴포넌트 분리

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,26 +1,12 @@
-import Card from "@/components/Card";
 import React from "react";
 import { getAllPosts } from "../api/posts";
+import FilterablePosts from "@/components/FilterablePosts";
 
-const posts = async () => {
+const postsPage = async () => {
   const allPosts = await getAllPosts();
+  const categories = [...new Set(allPosts.map((post) => post.category))];
 
-  return (
-    <div className="max-w-screen-xl mx-auto flex">
-      <div className="w-4/5 grid grid-cols-3 gap-4">
-        {allPosts.map((post, idx) => {
-          return <Card key={idx} post={post} />;
-        })}
-      </div>
-      <div className="w-1/5">
-        <div>Category</div>
-        <div>All Posts</div>
-        <div>My stroy</div>
-        <div>frontend</div>
-        <div>javascript</div>
-      </div>
-    </div>
-  );
+  return <FilterablePosts posts={allPosts} categories={categories} />;
 };
 
-export default posts;
+export default postsPage;
