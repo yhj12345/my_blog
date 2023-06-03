b61321c2ed30591cce53bf11e13d6f6f65407121
hojun <ghwnsdl999@naver.com>
2023-06-03 17:59:36 +0900

    [FE] add : post 라우팅

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Post } from "@/app/api/posts";
+import Link from "next/link";
 
 interface CardProps {
   post: Post;
@@ -7,20 +8,22 @@ interface CardProps {
 
 const Card = ({ post }: CardProps) => {
   return (
-    <div className="rounded border-2 border-gray-300">
-      <Image
-        width={1000}
-        height={50}
-        src={`/images/posts/${post.path}.png`}
-        alt="thumbnail"
-      />
-      <div className="flex flex-col items-center">
-        <time className="self-end">{post.date}</time>
-        <h2 className="font-bold">{post.title}</h2>
-        <p className="w-full truncate text-center">{post.description}</p>
-        <span className="bg-green-100 px-2 my-2">{post.category}</span>
+    <Link href={`/posts/${post.path}`}>
+      <div className="rounded border-2 border-gray-300">
+        <Image
+          width={1000}
+          height={50}
+          src={`/images/posts/${post.path}.png`}
+          alt="thumbnail"
+        />
+        <div className="flex flex-col items-center">
+          <time className="self-end">{post.date}</time>
+          <h2 className="font-bold">{post.title}</h2>
+          <p className="w-full truncate text-center">{post.description}</p>
+          <span className="bg-green-100 px-2 my-2">{post.category}</span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
